a6d08bcd2e5bc186a5db4987feadca5a693001a5
Vger0 <docarmoartur@gmail.com>
2024-09-30 14:44:14 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+
+namespace C__test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    string helloworld = "Hello, World, We came in peace!";
+        Console.WriteLine(helloworld);
+        int x = 20;
+        double y = 6.5;
+        Console.WriteLine( x + y );
+        char a = 'A';
+        char b = 'B';
+        Console.WriteLine(a + b);
+        float z = 3.14f;
+        Console.WriteLine(z * 4 + 100);
+        bool isOnline = false;
+        Console.WriteLine(isOnline);
+        long myNumber = 240000;
+        Console.WriteLine(myNumber);
+
+    }
+}
